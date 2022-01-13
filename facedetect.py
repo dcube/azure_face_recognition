9b7c339094a64e1e4f87453b6f6b88c24be51e6f
Hany Akoury <hany.akoury@gmail.com>
2022-01-13 13:29:40 +0100

    modify to suit everyone else

--- facedetect.py
+++ facedetect.py
@@ -22,3 +22,18 @@ KEY = ""
 
 # This endpoint will be used in all examples in this quickstart.
 ENDPOINT = ""
+
+# 1- Create an authenticated FaceClient.
+face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+# 2- Detect a face in an image that contains a single face
+single_face_image_url = "https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg"
+single_image_name = os.path.basename(single_face_image_url)
+
+detected_faces = face_client.face.detect_with_url(
+    url = single_face_image_url, detection_model='detection_03')
+
+if not detected_faces: 
+    raise Exception('No face dtected from image {}'.format(single_image_name))
+
+drawFaceRectangles(single_face_image_url,detected_faces)
